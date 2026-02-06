0fab34d4c69f47cc6dddfd77a2aeaca14a368b82
Landon <landonp95@gmail.com>
2026-02-05 19:03:46 -0500

    better argument aliasing, better color handling, and help printing for that

--- main.c
+++ main.c
@@ -23,6 +23,9 @@
 #include <time.h>
 #include <unistd.h>
 
+#define COLOR_BOLD "\e[1m"
+#define COLOR_OFF "\e[m"
+
 struct Drop {
     int delay;
     int current;
@@ -51,11 +54,64 @@ struct Config {
     int color;
 };
 
+struct StringAlias {
+    char *key;
+    int value;
+};
+
+struct StringAlias color_aliases[] = {
+    // blue
+    {"blue", COLOR_BLUE},
+    {"b", COLOR_BLUE},
+    // red
+    {"red", COLOR_RED},
+    {"r", COLOR_RED},
+    // green
+    {"green", COLOR_GREEN},
+    {"g", COLOR_GREEN},
+    // yellow
+    {"yellow", COLOR_YELLOW},
+    {"y", COLOR_YELLOW},
+    // white
+    {"white", COLOR_WHITE},
+    {"w", COLOR_WHITE},
+    {NULL, 0}};
+
+int get_color_from_alias(const char *input) {
+    for (int i = 0; color_aliases[i].key != NULL; i++) {
+        if (strcmp(input, color_aliases[i].key) == 0) {
+            return color_aliases[i].value;
+        }
+    }
+    return -1;
+}
+
 void help(struct Argument arguments[]) {
     printf("crain: the worst storm this side of the silicon\n\n");
 
     for (int i = 0; arguments[i].name != NULL; i++) {
-        printf("--%s, -%c: %s\n", arguments[i].name, arguments[i].alias, arguments[i].description);
+        // Skip hidden aliases (they are printed with their parent)
+        if (arguments[i].description == NULL)
+            continue;
+
+        // Print Main Flag
+        printf("--%s", arguments[i].name);
+
+        // Print Short Flag
+        if (arguments[i].alias != 0) {
+            printf(", -%c", arguments[i].alias);
+        }
+
+        // Print Aliases
+        for (int j = 0; arguments[j].name != NULL; j++) {
+            if (i == j)
+                continue; // Skip self
+            if (arguments[j].id == arguments[i].id) {
+                printf(", --%s", arguments[j].name);
+            }
+        }
+
+        printf(": %s\n", arguments[i].description);
     }
 
     exit(0);
@@ -68,8 +124,13 @@ int parse_args(struct Argument arguments[], struct Config *config, int argc,
         int matched = 0;
 
         for (int j = 0; arguments[j].name != NULL; j++) {
-            int is_short = (current[0] == '-' && current[1] == arguments[j].alias &&
+            // Check Short Flag (Only if alias is not 0)
+            int is_short = (arguments[j].alias != 0 &&
+                            current[0] == '-' &&
+                            current[1] == arguments[j].alias &&
                             current[2] == '\0');
+
+            // Check Long Flag (Matches --name)
             int is_long = (strncmp(current, "--", 2) == 0 &&
                            strcmp(current + 2, arguments[j].name) == 0);
 
@@ -79,22 +140,13 @@ int parse_args(struct Argument arguments[], struct Config *config, int argc,
                 switch (arguments[j].id) {
                 case ARG_COLOR:
                     if (i + 1 < argc) {
-                        switch (argv[i + 1][0]) {
-                        case 'b':
+                        int color_val = get_color_from_alias(argv[i + 1]);
+
+                        if (color_val != -1) {
+                            config->color = color_val;
+                        } else {
+                            fprintf(stderr, "Unknown color '%s'. Defaulting to blue.\n", argv[i + 1]);
                             config->color = COLOR_BLUE;
-                            break;
-                        case 'r':
-                            config->color = COLOR_RED;
-                            break;
-                        case 'g':
-                            config->color = COLOR_GREEN;
-                            break;
-                        case 'y':
-                            config->color = COLOR_YELLOW;
-                            break;
-                        case 'w':
-                            config->color = COLOR_WHITE;
-                            break;
                         }
                         i++;
                     }
@@ -105,20 +157,16 @@ int parse_args(struct Argument arguments[], struct Config *config, int argc,
                         config->character = argv[i + 1][0];
                         i++;
                     } else {
-                        fprintf(stderr, "Error: --character requires a value\n");
+                        fprintf(stderr, "Error: %s requires a value\n", current);
                         return 1;
                     }
                     break;
-
-                    // case ARG_NO_SPLASH:
-                    //     config->splash = 0;
-                    //     break;
 
                 case ARG_HELP:
                     help(arguments);
                     return 0;
                 }
-                break;
+                break; // Break inner loop (found match)
             }
         }
 
@@ -130,12 +178,50 @@ int parse_args(struct Argument arguments[], struct Config *config, int argc,
     return 0;
 }
 
+int snprintf_wrap(char *restrict s, size_t maxlen, const char *restrict format, const char *arg1, const char *arg2) {
+    size_t written = 0;
+    const char *args[2] = {arg1, arg2};
+    int toggle = 0;
+
+    for (const char *p = format; *p != '\0'; p++) {
+        if (*p == '%' && *(p + 1) == 's') {
+            const char *val = args[toggle];
+            toggle = !toggle;
+            p++;
+
+            while (*val) {
+                if (maxlen > 0 && written < maxlen - 1) {
+                    s[written] = *val;
+                }
+                written++;
+                val++;
+            }
+        } else {
+            if (maxlen > 0 && written < maxlen - 1) {
+                s[written] = *p;
+            }
+            written++;
+        }
+    }
+
+    if (maxlen > 0) {
+        s[written < maxlen ? written : maxlen - 1] = '\0';
+    }
+
+    return written;
+}
+
 int main(int argc, char **argv) {
     struct Config config = {.character = '|', .splash = 1, .color = COLOR_BLUE};
 
     if (argc > 1) {
-        struct Argument arguments[] = {{ARG_COLOR, "color", "set the color [b,r,g,y,w]", 'o'},
-                                       {ARG_CHARACTER, "character", "set the character", 'c'},
+        char color_desc[83];
+        snprintf_wrap(color_desc, 83, "set the color [%sb%slue, %sr%sed, %sg%sreen, %sy%sellow, %sw%shite]", COLOR_BOLD, COLOR_OFF);
+
+        struct Argument arguments[] = {{ARG_COLOR, "color", color_desc, 0},
+                                       {ARG_COLOR, "co", NULL, 0},
+                                       {ARG_CHARACTER, "character", "set the character", 0},
+                                       {ARG_CHARACTER, "ch", NULL, 0},
                                        //{ARG_NO_SPLASH, "no-splash", "disable splash", 's'},
                                        {ARG_HELP, "help", "view help", 'h'},
                                        {0, NULL, NULL, 0}};
